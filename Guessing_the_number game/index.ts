4f766fa602666e021accfd496b8470df49a01cb4
Owasis-tech55 <160662560+Owasis-tech55@users.noreply.github.com>
2024-03-23 01:30:07 +0500

    Add files via upload

---
+++ Guessing_the_number game/index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer"
+
+let randomNumber = Math.floor(Math.random() * 6 + 1)
+
+let number = await inquirer.prompt([
+    {
+    name: "usersnumber",
+    type: "number",
+    message: "please guess the number between 1 and 10"
+}
+])
+
+if(number.usersnumber === randomNumber){
+    console.log("Congtratulations, you guess the right number");
+
+}
+else{
+    console.log("You guess the wrong number, TRY AGAiN!!!");
+    
+}
+
+
